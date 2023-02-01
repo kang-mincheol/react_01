2133423e44b85a498ded78485a6e52f73eae651a
mincheol <aaa6315@gmail.com>
2023-02-01 14:24:16 +0900

    헤더 테스트

--- App.js
+++ App.js
@@ -8,13 +8,19 @@
 
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Header from './src/header';
 
 
 class App extends Component {
+
+  state = {
+    appName: 'My First App'
+  }
+
   render() {
     return (
       <View style={styles.mainView}>
-        <Text>hello world</Text>
+        <Header name={this.state.appName} />
       </View>
     )
   }
@@ -23,10 +29,31 @@ class App extends Component {
 const styles =  StyleSheet.create({
   mainView: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     paddingTop: 50,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+
+  subView: {
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+  },
+
+  anotherSubView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'normal',
+    color: 'red',
+    padding: 20
   }
 })
 
