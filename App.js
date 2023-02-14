834bae2559c04faae1437398ec6356e678094550
mincheol <aaa6315@gmail.com>
2023-02-14 11:42:58 +0900

    modal 스터디

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import NumList from './src/numlist';
 import Input from './src/input';
 import Picker from './src/picker';
 import ImageStudy from './assets/images/imageStudy.png';
+import Modal from './src/modal';
+
 
 
 class App extends Component {
@@ -41,12 +43,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.mainView}>
-        <Image
-          style={styles.image}
-          source={{uri: 'https://picsum.photos/id/237/200/300'}}
-          resizeMode='contain'
-          onLoadEnd={()=>alert('test')}
-        />
+        <Modal/>
       </View>
     )
   }
